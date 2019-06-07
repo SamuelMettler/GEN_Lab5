cb6fb84c181cb81e18b95235de8e490c931a9586
Mickael Bonjour <mickael.bonjour@heig-vd.ch@heig-vd.ch>
2019-06-07 10:41:59 -0500

    Remove build

--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mbonjour/HEIG/annee2/semestre_2/GEN/Labo/GEN_Lab5/test
-# Build directory: /home/mbonjour/HEIG/annee2/semestre_2/GEN/Labo/GEN_Lab5/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest-build")
