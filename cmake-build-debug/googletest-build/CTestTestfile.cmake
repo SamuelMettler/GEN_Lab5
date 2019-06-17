f8f68c924a0564f3197a13eb91e58db85c0b3bc0
Mickael Bonjour <mickael.bonjour@heig-vd.ch@heig-vd.ch>
2019-06-16 23:17:37 -0500

    Fixed DownloadProject

---
+++ cmake-build-debug/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mbonjour/HEIG/annee2/semestre_2/GEN/Labo/GEN_Lab5/cmake-build-debug/googletest-src
+# Build directory: /home/mbonjour/HEIG/annee2/semestre_2/GEN/Labo/GEN_Lab5/cmake-build-debug/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/test/CMakeFiles/runUnitTests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runUnitTests.dir/gtest.cpp.o"
+  "CMakeFiles/runUnitTests.dir/CustomerTest.cpp.o"
+  "CMakeFiles/runUnitTests.dir/MovieChildrensTest.cpp.o"
+  "CMakeFiles/runUnitTests.dir/MovieNewReleaseTest.cpp.o"
+  "CMakeFiles/runUnitTests.dir/MovieRegularTest.cpp.o"
+  "CMakeFiles/runUnitTests.dir/RentalTest.cpp.o"
+  "runUnitTests.pdb"
+  "runUnitTests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runUnitTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
